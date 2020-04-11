c560b75cf8c6a9d6e4ac72d1dc895d76186f1603
higorasilverio <higorasilverio@outlook.com>
2020-04-11 15:48:51 -0300

    Divide a selected number of times - Final

---
+++ src/application/Main.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("*** Divide a selected number of times ***");
+		System.out.println();
+		
+		System.out.print("Enter the quantity of divisions: ");
+		int control = scanner.nextInt();
+		System.out.println();
+		for  (int i = 0; i < control; i++) {
+			System.out.print("Enter the dividend: ");
+			double dividend = scanner.nextDouble();
+			System.out.print("Enter the divider: ");
+			double divider = scanner.nextDouble();
+			if (divider != 0) {
+				double quotient = dividend / divider;
+				System.out.printf("%.1f\n\n", quotient);
+			}
+			else System.out.printf("Imposible division!\n\n");
+		}
+		
+		scanner.close();
+		
+	}
+
+}
